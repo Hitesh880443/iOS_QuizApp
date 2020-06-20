1e025e3057aab59a7427b07953fbb5d753f33c20
Hitesh Vishwas Sutar <hsutar00@gmail.com>
2020-06-21 01:06:49 +0530

    first commit

---
+++ QuizApp/QuizStruct.swift
@@ -0,0 +1,14 @@
+//
+//  QuizStruct.swift
+//  QuizApp
+//
+//  Created by Hitesh, Vishwas Sutar on 20/06/20.
+//  Copyright © 2020 Hitesh Sutar. All rights reserved.
+//
+
+import Foundation
+
+struct QuizStruct {
+    let quetion : String
+    let answer : String
+}
